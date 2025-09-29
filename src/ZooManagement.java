bf139b4594e16a52dde86696af3b92c3ffa0f98a
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-09-29 19:12:16 +0100

    Instruction 1

---
+++ src/ZooManagement.java
@@ -0,0 +1,7 @@
+public class ZooManagement {
+    public static void main(String[] arg){
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        System.out.println(zooName + " comporte " +  nbrCages + " " + "cages");
+    }
+}

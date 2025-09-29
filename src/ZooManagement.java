06bcec274cb7b6a7208692ed719b91e4c21a4c9b
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-09-29 19:31:44 +0100

    Instruction 2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class ZooManagement {
     public static void main(String[] arg){
-        int nbrCages = 20;
-        String zooName = "my zoo";
-        System.out.println(zooName + " comporte " +  nbrCages + " " + "cages");
+        int nbrCages;
+        String zooName;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Donnez le nom de zoo : ");
+        zooName = sc.nextLine();
+        if(zooName.isEmpty()){
+            System.out.println("Le nom du zoo ne peut pas etre vide");
+        }
+
+        System.out.println("Donnez le nombre de cages : ");
+        nbrCages = sc.nextInt();
+        if(nbrCages <=0){
+            System.out.println("Le nombre de cages doit etre supérieur à 0");
+        }
+
     }
 }

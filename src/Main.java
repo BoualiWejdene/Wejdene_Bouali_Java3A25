d3d389f6825487ed43284ed256a2cb53ddb8be3f
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-09-29 19:49:07 +0100

    Instruction 5

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Animal lion = new Animal();
+        Zoo myZoo= new Zoo();
+
+        lion.name = "simba";
+        lion.family = "pretador";
+        lion.age = 25;
+        lion.isMammal = true;
+
+
+        myZoo.name = "Zoo de tunis";
+        myZoo.city = "tunis";
+        myZoo.nbrCages = 20;
+
+    }
+}

654ea7648635ede9a925396f35c41a1ac225e694
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-09-29 19:36:10 +0100

    Instruction 3

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -8,14 +8,16 @@ public class ZooManagement {
 
         System.out.println("Donnez le nom de zoo : ");
         zooName = sc.nextLine();
-        if(zooName.isEmpty()){
-            System.out.println("Le nom du zoo ne peut pas etre vide");
-        }
 
         System.out.println("Donnez le nombre de cages : ");
         nbrCages = sc.nextInt();
-        if(nbrCages <=0){
+
+        if(zooName.isEmpty()){
+            System.out.println("Le nom du zoo ne peut pas etre vide");
+        }else if(nbrCages <=0){
             System.out.println("Le nombre de cages doit etre supérieur à 0");
+        }else{
+            System.out.println(zooName + " comporte " +  nbrCages + " " + "cages");
         }
 
     }

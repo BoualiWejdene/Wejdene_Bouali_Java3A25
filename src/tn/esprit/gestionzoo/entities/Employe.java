c9bdaa3fa15e24f911c31a34b18a9c61c9f83ed7
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-11-08 20:22:46 +0100

    Classe Employe

---
+++ src/tn/esprit/gestionzoo/entities/Employe.java
@@ -0,0 +1,71 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Employe {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String nomDepartement;
+    private int grade;
+
+    public Employe() {
+    }
+
+    public Employe(int grade, String prenom, String nomDepartement, String nom, int id) {
+        this.grade = grade;
+        this.prenom = prenom;
+        this.nomDepartement = nomDepartement;
+        this.nom = nom;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNomDepartement() {
+        return nomDepartement;
+    }
+
+    public void setNomDepartement(String nomDepartement) {
+        this.nomDepartement = nomDepartement;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", nomDepartement='" + nomDepartement + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}

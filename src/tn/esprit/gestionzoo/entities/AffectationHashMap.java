14cce700b1f28a005fc5e46678f26eb21a2cbf16
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2025-11-22 08:55:38 +0100

    1. Classe AffectationHashMap

---
+++ src/tn/esprit/gestionzoo/entities/AffectationHashMap.java
@@ -0,0 +1,13 @@
+package tn.esprit.gestionzoo.entities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AffectationHashMap {
+    private Map<Employe, Departement> affectations;
+
+    public AffectationHashMap() {
+        affectations = new HashMap<>();
+    }
+
+}
